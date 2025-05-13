4ab790ed0cb127fde0c6a31bf40f7bbf870e4f48
CarlosEGoulart <goulart193@gmail.com>
2025-05-12 23:20:27 -0300

    Upd POO_2025.1 Enum, Injection, Jest

--- dist/Jest.test.js
+++ dist/Jest.test.js
@@ -1,1 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Art_1 = __importDefault(require("./model/Art"));
+const Artist_1 = __importDefault(require("./model/Artist"));
+const Exhibition_1 = __importDefault(require("./model/Exhibition"));
+test("Test Art class", () => {
+    const art = new Art_1.default(1, "Starry Night", "A painting by Vincent van Gogh", 1889);
+    expect(art.getId()).toBe(1);
+    expect(art.getName()).toBe("Starry Night");
+    expect(art.getDescription()).toBe("A painting by Vincent van Gogh");
+    expect(art.getYear()).toBe(1889);
+});
+test("Test Artist class", () => {
+    const artist = new Artist_1.default(1, "Vincent van Gogh", "Dutch post-impressionist painter", 1853, "@vangogh");
+    expect(artist.getId()).toBe(1);
+    expect(artist.getName()).toBe("Vincent van Gogh");
+    expect(artist.getBio()).toBe("Dutch post-impressionist painter");
+});
+test("Test Exhibition class", () => {
+    const exhibition = new Exhibition_1.default(1, "Impressionist Art", "A collection of impressionist paintings", [1, 2]);
+    expect(exhibition.getId()).toBe(1);
+    expect(exhibition.getName()).toBe("Impressionist Art");
+    expect(exhibition.getDescription()).toBe("A collection of impressionist paintings");
+    expect(exhibition.getArtWorks()).toEqual([1, 2]);
+});
