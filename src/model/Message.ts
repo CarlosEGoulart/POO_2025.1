98c59fdae18d77f21f6e5532cbfd83b053cf6e8b
Carlos Eduardo Goulart <142110393+CarlosEGoulart@users.noreply.github.com>
2025-05-24 13:44:30 -0300

    Update Message.ts

--- src/model/Message.ts
+++ src/model/Message.ts
@@ -1,7 +1,7 @@
 import { MessageType } from "./EnumType";
 import IMessage from "./IMessage";
 
-export default class Fit implements IMessage {
+export default class Message implements IMessage {
     public showMessage(type: MessageType): void {
         console.log(type);
     }
